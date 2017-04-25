e5de78bc3ee7573686f9a5b867931413f494354c
Abram Sanderson <abram.sanderson@gmail.com>
2017-04-25 10:42:55 -0700

    Update item getter.

--- item_getter.py
+++ item_getter.py
@@ -6,7 +6,8 @@ from datetime import datetime, timedelta
 from riotwatcher import LoLException, error_404, error_429
 import time
 
-key = 'RGAPI-dc4a4568-8f39-4536-8cbd-5dd767808a15'
+with open('key.txt') as f:
+	key = f.read()
 
 rw = RiotWatcher(key)
 item_list = rw.static_get_item_list()
